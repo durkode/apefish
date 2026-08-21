6e0874ae901a352ce75de8c5429cbcb91fd449ad
Hamish Durkin <hamish@durk.in>
2026-08-21 17:25:08 +1000

    getters for zobrist randoms

--- engine/src/zobrist.rs
+++ engine/src/zobrist.rs
@@ -1,18 +1,19 @@
 
 use rand::{RngExt, SeedableRng, random};
 use rand_chacha::ChaChaRng;
-use crate::basetypes::{CastlingRights, PerPiece, PerSide, PerSquare};
+use crate::{PieceKind, Square, basetypes::{CastlingRights, PerPiece, PerSide, PerSquare, Side}};
 
 
-type ZR = u64;
+type ZobristKey = u64;
 const RNG_SEED: [u8; 32] = [54; 32];
 
 pub struct ZobristRandoms {
-    piece_randoms: PerSide<PerPiece<PerSquare<ZR>>>,
-    side_randoms: PerSide<ZR>,
-    castling_randoms: [ZR; CastlingRights::NUM_RIGHTS_COMBOS],
-    ep_square_randoms: PerSquare<ZR>,
-    ep_empty_random: ZR,
+    piece_randoms: PerSide<PerPiece<PerSquare<ZobristKey>>>,
+    side_randoms: PerSide<ZobristKey>,
+    castling_randoms: [ZobristKey; CastlingRights::NUM_RIGHTS_COMBOS],
+    // Technically this could be much smaller, but easier to just do for every square.
+    ep_square_randoms: PerSquare<ZobristKey>,
+    ep_empty_random: ZobristKey,
 }
 
 impl ZobristRandoms {
@@ -22,20 +23,20 @@ impl ZobristRandoms {
 
         let piece_randoms = PerSide::from_array(std::array::from_fn(|_|
             PerPiece::from_array(std::array::from_fn(|_|
-                PerSquare::from_array(std::array::from_fn(|_| random.random::<ZR>()))
+                PerSquare::from_array(std::array::from_fn(|_| random.random::<ZobristKey>()))
             ))
         ));
 
         let side_randoms = PerSide::from_array(
-            std::array::from_fn(|_| random.random::<ZR>())
+            std::array::from_fn(|_| random.random::<ZobristKey>())
         );
         
-        let castling_randoms: [ZR; CastlingRights::NUM_RIGHTS_COMBOS] = std::array::from_fn(|_| random.random::<ZR>());
+        let castling_randoms: [ZobristKey; CastlingRights::NUM_RIGHTS_COMBOS] = std::array::from_fn(|_| random.random::<ZobristKey>());
         
         let ep_square_randoms = PerSquare::from_array(
-            std::array::from_fn(|_| random.random::<ZR>())
+            std::array::from_fn(|_| random.random::<ZobristKey>())
         );
-        let ep_empty_random = random.random::<ZR>();
+        let ep_empty_random = random.random::<ZobristKey>();
 
         ZobristRandoms { 
             piece_randoms, 
@@ -45,4 +46,24 @@ impl ZobristRandoms {
             ep_empty_random 
         }
     }
+
+    pub fn piece_key(&self, side: Side, piece_kind: PieceKind, square: Square) -> ZobristKey {
+        self.piece_randoms[side][piece_kind][square]
+    }
+
+    pub fn side_key(&self, side: Side) -> ZobristKey {
+        self.side_randoms[side]
+    }
+
+    pub fn castling_key(&self, rights: u8) -> ZobristKey {
+        self.castling_randoms[rights as usize]
+    }
+
+    pub fn ep_key(&self, ep_square: Option<Square>) -> ZobristKey {
+        match ep_square {
+            Some(s) => self.ep_square_randoms[s],
+            None => self.ep_empty_random
+        }
+    }
+
 }
